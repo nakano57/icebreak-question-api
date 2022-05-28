3f541793735e091e439384c3dd64488f70c272de
nakano57 <hiroto856@gmail.com>
2022-05-28 16:52:04 +0900

    a

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 type Question struct {
-	QUESTION string `json:"question"`
-	CATEGORY string `json:"category"`
+	QUESTION string   `json:"question"`
+	CATEGORY string   `json:"category"`
+	CHOICES  *Choices `json:"choices"`
+}
+
+type Choices struct {
+	First  string `json:"first"`
+	Second string `json:"second"`
 }
 
 var q []Question
@@ -25,10 +32,10 @@ func main() {
 	r := mux.NewRouter()
 
 	//テストデータ
-	q = append(q, Question{QUESTION: "元気ですかーッ！", CATEGORY: "体調"})
+	q = append(q, Question{QUESTION: "元気ですかーッ！", CATEGORY: "体調", CHOICES: &Choices{First: "元気です", Second: "めっちゃ元気です!!!!!!"}})
 
 	// ルート(エンドポイント)
 	r.HandleFunc("/api/random", getQuestions).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
 }
