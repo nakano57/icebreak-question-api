b395663b05d2ec7e01812cf22619e9887709f728
nakano57 <hiroto856@gmail.com>
2022-05-28 21:23:38 +0900

    a

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -16,76 +16,35 @@ import (
 
 var token string
 
-app := &kintone.App{
-	Domain:   "out8j59wrrrm.cybozu.com",
-	User:     "user1",
-	Password: "password",
-	AppId:    25,
-}
-
-type Parameters struct {
-}
-
 func getQuestions(w http.ResponseWriter, r *http.Request) {
 	//kintone
-	jsonStr := `{
-		"app": 2,
-		"id": 1,
-		"query": "isUse in (\"使用\")",
-		"fields": ["question","category", "first", "second"]}`
-	url := "https://out8j59wrrrm.cybozu.com/k/v1/records.json"
-	req, _ := http.NewRequest("GET", url, bytes.NewBuffer([]byte(jsonStr)))
-	req.Header.Set("X-Cybozu-API-Token", token)
-	req.Header.Add("Content-Type", "application/json")
-
-	client := new(http.Client)
-	resp, _ := client.Do(req)
-
-	defer resp.Body.Close()
-
-	response, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("レスポンス")
-	fmt.Println(string(response))
-
-	//うまくいかない
-	if err := json.Unmarshal([]byte(response), &qs); err != nil {
-		panic(err)
+	app := &kintone.App{
+		Domain:   "out8j59wrrrm.cybozu.com",
+		ApiToken: token,
+		AppId:    2,
 	}
 
-	//fmt.Println(qs["records"])
-	mapData := qs.(map[string]interface{})
-	delete(mapData, "totalCount")
-
-	jsonStr2, err := json.Marshal(mapData)
+	fields := []string{"question", "category", "first", "second"}
+	records, err := app.GetRecords(fields, "isUse in (\"使用\")")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-
-	fmt.Println("Totalcount削除")
-	fmt.Println(string(jsonStr2))
-
-	qs3 := make([]*Question, 0)
-
-	//うまくいかない
-	if err := json.Unmarshal([]byte(jsonStr2), &qs3); err != nil {
-		panic(err)
-	}
-
-	fmt.Println(qs3)
+	n := rand.Intn(len(records))
+	question := records[n]
+	fmt.Println(question.Fields)
 
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-	json.NewEncoder(w).Encode(qs3)
+	json.NewEncoder(w).Encode(records[n])
 }
 
 func main() {
 	//Token周り
+	rand.Seed(time.Now().UnixNano())
 	err := godotenv.Load(".env")
 
-	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
 	if err != nil {
 		fmt.Printf("ファイルが存在しません: %v", err)
 	}
-
 	token = os.Getenv("TOKEN")
 
 	// ルーターのイニシャライズ
